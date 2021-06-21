4c23d3b4e093fdaddad0d71da6b535995cf0ece3
chenl <genec_wy@163.com>
2021-06-21 10:48:42 +0800

    公司

---
+++ BASE/day05-for.py
@@ -0,0 +1,10 @@
+#for 循环   break使用
+str =  '我的中国心, my love'
+for i  in  str:
+    #遇到空格和o不打印,跳过
+    if i ==' 'or i == 'o':
+        continue
+    elif i == 'e':
+        break
+    else:
+        print(i)
