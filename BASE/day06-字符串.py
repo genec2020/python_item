cb0dce388beaf6037b5b92e082379c6e38169f02
chenl <genec_wy@163.com>
2021-06-24 20:01:11 +0800

    公司

--- BASE/day06-字符串.py
+++ BASE/day06-字符串.py
@@ -1,1 +1,12 @@
-#字符串相关
+#字符串  3种创建
+str1 = '我的家'
+str2 = "我的家"
+str3 = ''' 我爱我的家,to english
+                i love  my home'''
+print(type(str3))
+print(str3)
+
+#切片   默认步长为1
+new_str3 = str3[2:5:1]
+print(new_str3)
+#步长2是什么?
