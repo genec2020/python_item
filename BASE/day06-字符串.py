c04252f34cd79e4bda1915ff2d9e103e57e4484b
chenl <genec_wy@163.com>
2021-07-17 08:00:08 +0800

    公司

--- BASE/day06-字符串.py
+++ BASE/day06-字符串.py
@@ -1,7 +1,7 @@
 #字符串  3种创建
 str1 = '我的家'
 str2 = "我的家"
-str3 = ''' 我爱我的家,to english
+str3 = '''我爱我的家,to english
                 i love  my home'''
 print(type(str3))
 print(str3)
@@ -10,4 +10,23 @@ print(str3)
 new_str3 = str3[2:5:1]
 print(new_str3)
 
-#步长2是什么?
+#步长2是什么? 控制截取顺序
+new_str4 = str3[1:4:2]
+print(new_str4+"===")
+
+new_str5 = str3[1:4:-2] #无法获取数据
+print(new_str5+"-2")
+
+new_str6 = str3[-1:-4:-2]
+print(new_str6+"-22")
+
+#find   index  字符第一次出现位置下标
+str = "hello world and  home and wonder"
+num  =  str.find('and')
+print(num)
+
+#查找最后一次出现的字串, 返回位置值
+num  =  str.rfind('and')
+print(num)
+
+
